875e1a79cc9ec996c21b23b8b197ecaa20f0bb41
Alaamhmdgalal <alaaemran99@gmail.com>
2022-04-22 13:20:42 +0200

    Watermelon Class and its test class

---
+++ src/com/company/Watermelon.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+
+public class Watermelon {
+
+    public static String isEven(int n){
+        if ( n % 2 == 0 && n != 2)
+            return "YES";
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int w = input.nextInt();
+        System.out.println(isEven(w));
+    }
+}
