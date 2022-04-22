cbf30b541493cf656921b2e6232587f8cd6e1b6c
Alaamhmdgalal <alaaemran99@gmail.com>
2022-04-22 19:48:08 +0200

    Code after failed test cases fixation

---
+++ src/com/company1/YoungPhysicist.java
@@ -0,0 +1,39 @@
+package com.company1;
+import java.util.*;
+
+public class YoungPhysicist {
+
+    public static String isEquilibrium(int [][] array2D, int n){
+        if (n == 0 || n < 0 || n > 100)
+            System.out.println("NO");
+
+        else {
+            for( int i=0; i<3; i++){
+                int sum=0;
+                for(int j=0; j<n; j++){
+                    if(array2D[i][j] > 100 || array2D[i][j] < -100)
+                    return "NO";
+                    else
+                    sum+=array2D[j][i];
+                }
+                if(sum == 0)
+                    return "YES";
+                return "NO";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int array2D[][] = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                array2D[i][j] = input.nextInt();
+            }
+        }
+        System.out.println(isEquilibrium(array2D, n));
+    }
+}
