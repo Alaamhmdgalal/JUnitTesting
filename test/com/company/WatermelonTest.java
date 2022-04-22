083c2c7b8c7aca67644e0a6275b144af693546fd
Alaamhmdgalal <alaaemran99@gmail.com>
2022-04-22 13:22:26 +0200

    Watermelon Class and its test class

---
+++ test/com/company/WatermelonTest.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WatermelonTest {
+
+    @Test
+    public void test1() {
+        Watermelon tester = new Watermelon();
+        assertEquals("YES",tester.isEven(8));
+    }
+    @Test
+    public void test2() {
+        Watermelon tester = new Watermelon();
+        assertEquals("NO", tester.isEven(7));
+    }
+    @Test
+    public void test3() {
+        Watermelon tester = new Watermelon();
+        assertEquals("NO", tester.isEven(2));
+    }
+
+}
